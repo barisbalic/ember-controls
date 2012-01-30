2d74ef63a47f862eaeed4ddd46c79b1a3093573b
Petr Janda <petrjanda@me.com>
2012-01-31 00:18:32 +0100

    use getter instead

--- packages/ember-extviews/lib/submit_button.js
+++ packages/ember-extviews/lib/submit_button.js
@@ -2,6 +2,6 @@
  * Submit button to be used to trigger the formular send action.
  */
 Ember.SubmitButton = Ember.Button.extend({
-  target: parentView,
+  target: this.get('parentView'),
   action: 'submit'
 })
